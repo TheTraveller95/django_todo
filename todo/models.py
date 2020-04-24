6d4f8cbdb918cf32256e24557eafba3fcfddb3d3
TheTraveller95 <crivellaroandres@gmail.com>
2020-04-24 09:35:29 +0000

    add all test functions

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,5 @@ class Item(models.Model):
     name = models.CharField(max_length=30, blank=False)
     done = models.BooleanField(blank=False, default=False)
 
+    def __str__(self):
+        return self.name
